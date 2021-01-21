5f6b38bdb6fe66654d4fb335c930fb29d1c549c8
Amber Teetsel <amberteetsel@Ambers-MacBook-Pro.local>
2021-01-21 15:12:17 -0700

    Employee & manager info

--- schema.sql
+++ schema.sql
@@ -131,3 +131,43 @@ FROM current_emp AS ce
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no ASC;
+
+-- List 1: Retirees incl. Employee #, last name, first name, gender, salary
+
+-- Make sure salaries to_date aligns with dept_emp to_date (it doesn't)
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+	INNER JOIN salaries AS s
+ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_employees AS de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = ('9999-01-01'));
+
+-- List 2: Management
+-- Manager's emp_no, department name/#, first name, last name, start and end dates
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.first_name,
+	ce.last_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+
+--
