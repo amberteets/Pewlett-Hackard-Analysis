bf30ff342b2746fdf020cef4461f87ff4d347a06
Amber Teetsel <amberteetsel@Ambers-MacBook-Pro.local>
2021-01-21 14:41:09 -0700

    Retirement info by department

--- schema.sql
+++ schema.sql
@@ -82,4 +82,52 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+SELECT * FROM dept_employees;
+
+-- Update retirement_info to include employee #, dept #
+-- My version
+DROP TABLE retirement_info;
+
+SELECT de.emp_no, de.dept_no, e.first_name, e.last_name 
+INTO retirement_info
+FROM employees AS e JOIN dept_employees AS de
+	ON e.emp_no = de.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 SELECT * FROM retirement_info;
+
+-- Update retirement table to include employee #, first/last name, and if they are
+-- currently employed with the company (module version)
+DROP TABLE retirement_info;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+	-- Now join with dept_employees table for if they left company
+SELECT ri.emp_no, ri.first_name, ri.last_name, de.to_date
+FROM retirement_info AS ri LEFT JOIN dept_employees AS de
+	ON ri.emp_no = de.emp_no
+	
+-- Create current_emp table for retirement-eligible workers still at company
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+	LEFT JOIN dept_employees AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Retiree employee count by department
+SELECT de.dept_no, COUNT(ce.emp_no)
+INTO retirement_dept
+FROM current_emp AS ce
+	LEFT JOIN dept_employees AS de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no ASC;
